96f1009d1b9f32c782b6054d0f91d2903d5b1314
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2026-06-05 21:38:18 -0500

    feat: login redirect if loggedin

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,6 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { LoginPage } from "@/components/pages/login-page";
+import { getSession } from "@/lib/auth/functions";
 
 export const Route = createFileRoute("/login")({
   component: LoginPage,
+  beforeLoad: async () => {
+    const session = await getSession();
+    if (session) {
+      throw redirect({
+        to: "/",
+      });
+    }
+  },
 });
